436d68d0265aeb89c18a18c39d1c4f6e41f70973
loder-coder <syinfo4@gmail.com>
2026-01-05 15:18:12 +0900

    디벨로퍼용 테스트 1

---
+++ services/user_service.py
@@ -0,0 +1,34 @@
+import logging
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User
+from sqlalchemy.exc import IntegrityError
+
+logger = logging.getLogger(__name__)
+
+class UserService:
+    @staticmethod
+    def create_user(username, password) -> bool:
+        try:
+            password_hash = generate_password_hash(password)
+            new_user = User(id=username, password_hash=password_hash)
+            db.session.add(new_user)
+            db.session.commit()
+            return True
+        except IntegrityError:
+            db.session.rollback()
+            return False
+        except Exception as e:
+            logger.error(f"Create User Error: {e}")
+            db.session.rollback()
+            return False
+
+    @staticmethod
+    def verify_user(username, password):
+        try:
+            user = User.query.get(username)
+            if user and check_password_hash(user.password_hash, password):
+                return user
+            return None
+        except Exception as e:
+            logger.error(f"Verify User Error: {e}")
+            return None
